997cab88d9af5bbb7cc8bbd86d740d9523e53cd9
darshanaMadushan <darshanamsamarasinghe@gmail.com>
2022-02-01 09:32:06 +0530

    removed old project

--- KLASSMATE Inventory/src/main/java/com/rcs2/inventory/model/Status.java
+++
@@ -1,5 +0,0 @@
-package com.rcs2.inventory.model;
-
-public enum Status {
-	IN_INVENTORY, ON_ASSEMBLE, ALLOCATED, FINISHED, REPLACED;
-}
--- KLASSMATE Inventory/src/main/resources/static/js/navbar.js
+++
@@ -1,7 +0,0 @@
-const requestLink = document.getElementById("requestProduct");
-
-requestLink.addEventListener("click", function(){
-	
-	console.log(requestLink.innerText);
-	
-})
